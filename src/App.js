2da69ea0e5c2a77a26cc1d0e83d8fd9a2497f39d
sedx876 <swatkins876@gmail.com>
2020-09-02 13:52:24 -0400

    Home and Staff components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Staff from './components/pages/Staff'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      
+      <Navbar />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/staff" exact={true} component={Staff} />
     </div>
+    </Router>
   );
 }
 
