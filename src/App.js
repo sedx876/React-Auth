95beccae2c4ec2e2f84d87d7b88d0c947edba5c5
sedx876 <swatkins876@gmail.com>
2020-09-03 10:02:05 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -4,15 +4,38 @@ import Navbar from './components/layout/Navbar'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Staff from './components/pages/Staff'
+import Login from './components/auth/Login'
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+
+
+function onAuthRequired({ history }) {
+  history.push('/login');
+}
 
 function App() {
   return (
     <Router>
+    <Security 
+      issuer='https://dev-190132.okta.com/oauth2/default'
+      clientId='0oavbw80969fRFTOS4x6'
+      redirectUri={window.location.origin + '/implicit/callback'}
+      onAuthRequired={onAuthRequired}
+      pkce={true}>
     <div className="App">
       <Navbar />
+      <div className="container">
       <Route path="/" exact={true} component={Home} />
-      <Route path="/staff" exact={true} component={Staff} />
+      <SecureRoute path="/staff" exact={true} component={Staff} />
+      <Route
+                path="/login"
+                render={() => (
+                  <Login baseUrl="https://dev-190132.okta.com" />
+                )}
+              />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
+      </div>
     </div>
+    </Security>
     </Router>
   );
 }
